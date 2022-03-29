00f4bdf58a067a5a19dcc0b1169b2e33bcced38f
Aleksandar Lekov <56325705+shonie2xx@users.noreply.github.com>
2022-03-29 13:34:13 +0200

    initial

---
+++ src/test/java/com/careerjumpstart/admin_ms/AdminMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerjumpstart.admin_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
