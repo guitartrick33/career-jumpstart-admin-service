76334f3531fe0b79e4f699ecef1341bba78f844f
Georgi2704 <jrmanev@gmail.com>
2022-06-08 11:27:49 +0200

    cross origins for the new domain

--- src/main/java/com/careerjumpstart/admin_ms/controllers/AnswerController.java
+++ src/main/java/com/careerjumpstart/admin_ms/controllers/AnswerController.java
@@ -15,7 +15,8 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:3000", "https://careerjumpapp.com"}, maxAge = 3600, allowCredentials = "true")
+
 @RequiredArgsConstructor
 
 @RequestMapping("/admin/answers")
--- src/main/java/com/careerjumpstart/admin_ms/controllers/QuestionController.java
+++ src/main/java/com/careerjumpstart/admin_ms/controllers/QuestionController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:3000", "https://careerjumpapp.com"}, maxAge = 3600, allowCredentials = "true")
 @RequiredArgsConstructor
 @RequestMapping("/admin/question")
 public class QuestionController {
